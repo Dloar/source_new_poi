710464899636e7210e641fe6c39f20a2c2608519
dloar <ondrej_kral@hotmail.com>
2024-04-20 18:14:55 +0300

    main: Execute from bash

--- src/bash_files/find_new_poi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export poi_brand=$1
-export poi_group=$2
-source /home/ubuntu/adwiro-proj/source_new_poi/venv/bin/activate
-export PYTHONPATH=${PYTHONPATH}:~/adwiro-proj
-
-python3 /home/ubuntu/adwiro-proj/source_new_poi/src/commands/__main__.py poi_brand poi_group
