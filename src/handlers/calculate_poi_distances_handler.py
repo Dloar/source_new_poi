d6f9576764ba6ac244047e444d09b19ca3792b2f
dloar <ondrej_kral@hotmail.com>
2024-04-20 20:22:02 +0300

    main: config reference

--- src/handlers/calculate_poi_distances_handler.py
+++ src/handlers/calculate_poi_distances_handler.py
@@ -17,23 +17,26 @@ class CalculatedPoiDistances:
             valid_distance_list = list()
 
         # iterate over new pois and calculated distance
-        for new_poi in selected_poi_df.new_ids_df.index:
-            selected_poi = selected_poi_df.new_ids_df.loc[new_poi]
-            for billboard in billboard_df.index:
-                selected_board = billboard_df.iloc[billboard]
-                if (selected_board['latitude'] is None) | (selected_board['longitude'] is None) | (
-                        selected_board['latitude'] == '') | (selected_board['longitude'] == ''):
-                    continue
-                distance = calculate_distance_between_two_points(point_lat1=selected_poi['lat'],
-                                                                 point_long1=selected_poi['long'],
-                                                                 point_lat2=selected_board['latitude'],
-                                                                 point_long2=selected_board['longitude'])
-                if distance > 5:
-                    continue
-                valid_distance_list = valid_distance_list + [{'poi_index': new_poi,
-                                                              'billboard_id': selected_board['billboard_id'],
-                                                              'distance': distance}]
-            print(f'All boards done for {new_poi}')
+        try:
+            for new_poi in selected_poi_df.new_ids_df.index:
+                selected_poi = selected_poi_df.new_ids_df.loc[new_poi]
+                for billboard in billboard_df.index:
+                    selected_board = billboard_df.iloc[billboard]
+                    if (selected_board['latitude'] is None) | (selected_board['longitude'] is None) | (
+                            selected_board['latitude'] == '') | (selected_board['longitude'] == ''):
+                        continue
+                    distance = calculate_distance_between_two_points(point_lat1=selected_poi['lat'],
+                                                                     point_long1=selected_poi['long'],
+                                                                     point_lat2=selected_board['latitude'],
+                                                                     point_long2=selected_board['longitude'])
+                    if distance > 5:
+                        continue
+                    valid_distance_list = valid_distance_list + [{'poi_index': new_poi,
+                                                                  'billboard_id': selected_board['billboard_id'],
+                                                                  'distance': distance}]
+                print(f'All boards done for {new_poi}')
+        except:
+            pass
         return valid_distance_list
 
     def define_new_poi_dataframe(self):
