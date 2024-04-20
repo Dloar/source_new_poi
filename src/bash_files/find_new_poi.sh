b9164a73eb465c42432b2e7779af0a0af3d8c4f3
dloar <ondrej_kral@hotmail.com>
2024-04-20 18:11:06 +0300

    main: Execute from bash

--- src/bash_files/find_new_poi.sh
+++ src/bash_files/find_new_poi.sh
@@ -5,4 +5,4 @@ export poi_group=$2
 source /home/ubuntu/adwiro-proj/source_new_poi/venv/bin/activate
 export PYTHONPATH=${PYTHONPATH}:~/adwiro-proj
 
-python3 /home/ubuntu/adwiro-proj/source_new_poi/src/command/__main__.py poi_brand poi_group
+python3 /home/ubuntu/adwiro-proj/source_new_poi/src/commands/__main__.py poi_brand poi_group
