61e4a41e8ad005e2b481a92575f56aaa1ddd125b
dloar <ondrej_kral@hotmail.com>
2024-04-20 19:28:15 +0300

    main: config reference

--- src/commands/find_poi_on_name_handler.py
+++ src/commands/find_poi_on_name_handler.py
@@ -7,7 +7,7 @@ import yaml
 
 class FindPoiOnNameHandler():
     def __init__(self, ):
-        with open('../../development/config.yaml') as config:
+        with open('../development/config.yaml') as config:
             country_parts_config = yaml.safe_load(config)
 
         self.districts = country_parts_config['model_parameters']['country_districts']
@@ -73,5 +73,5 @@ class FindPoiOnNameHandler():
         selected_poi_df.drop_duplicates(inplace=True)
         selected_poi_df.reset_index(drop=True, inplace=True)
 
-        print(f'For {poi_name}, the model found {selected_poi_df.shape[0]} places')
+        logging.info(f'For {poi_name}, the model found {selected_poi_df.shape[0]} places')
         return selected_poi_df
