3e2e3cffe1fd8ba54731a06f0b327c82ad454e99
dloar <ondrej_kral@hotmail.com>
2024-04-20 20:23:53 +0300

    main: config reference

--- src/services/calculate_distance_between_two_points_service.py
+++ src/services/calculate_distance_between_two_points_service.py
@@ -2,7 +2,6 @@ import mpu
 
 
 def calculate_distance_between_two_points(point_lat1, point_lat2, point_long1, point_long2):
-    print(f'{point_lat1}, {point_lat2}, {point_long1}, {point_long2}')
     distance = mpu.haversine_distance((float(point_lat1),
                                        float(point_long1)),
                                       (float(point_lat2),
